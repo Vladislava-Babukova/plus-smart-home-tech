50c8fee445d725c2050ff83f50eff7909876da82
Vladislava Babukova <Vlada.409b@gmail.com>
2025-11-22 20:38:25 +0300

    aggregator

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/hub/BaseHubEventHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/hub/BaseHubEventHandler.java
@@ -1,4 +1,5 @@
 package ru.yandex.practicum.telemetry.collector.service.hub;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.specific.SpecificRecordBase;
