deec48064ae8f54fa40aaff4f0d16a0ad6df7e6c
Vladislava Babukova <Vlada.409b@gmail.com>
2025-11-22 21:34:58 +0300

    aggregator3

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/SensorEventDeserializer.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.telemetry.aggregator;
+
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.io.Decoder;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Map;
+
+public class SensorEventDeserializer implements Deserializer<SensorEventAvro> {
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        // Nothing to configure
+    }
+
+    @Override
+    public SensorEventAvro deserialize(String topic, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+        try {
+            SpecificDatumReader<SensorEventAvro> reader = new SpecificDatumReader<>(SensorEventAvro.getClassSchema());
+            Decoder decoder = DecoderFactory.get().binaryDecoder(data, null);
+            return reader.read(null, decoder);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to deserialize SensorEventAvro", e);
+        }
+    }
+
+    @Override
+    public void close() {
+        // Nothing to close
+    }
+}
