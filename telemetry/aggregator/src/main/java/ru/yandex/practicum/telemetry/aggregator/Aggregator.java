f09654c577df6b291ef81c626092c9e7a02f3695
Vladislava Babukova <Vlada.409b@gmail.com>
2025-11-25 22:57:03 +0300

    20-фз. Исправлены замечания.

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/Aggregator.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/Aggregator.java
@@ -3,9 +3,11 @@ package ru.yandex.practicum.telemetry.aggregator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableAsync
 public class Aggregator {
     public static void main(String[] args) {
         SpringApplication.run(Aggregator.class, args);
