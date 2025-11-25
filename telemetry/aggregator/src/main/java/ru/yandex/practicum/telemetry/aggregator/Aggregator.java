365bcf96a009b7faea2d3c983dee4a6d18ee19cf
Vladislava Babukova <Vlada.409b@gmail.com>
2025-11-25 23:03:24 +0300

    20-фз. Исправлены замечания.

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/Aggregator.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/Aggregator.java
@@ -3,13 +3,28 @@ package ru.yandex.practicum.telemetry.aggregator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableAsync
 public class Aggregator {
     public static void main(String[] args) {
-        SpringApplication.run(Aggregator.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Aggregator.class, args);
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            latch.countDown();
+        }));
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
